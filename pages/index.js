4b7ca80e40ef457463b6478cd0a6a0098d2c4bdb
Richard Narvaez <richard.bvn997@gmail.com>
2021-01-01 20:01:23 -0500

    🚀 First commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,77 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { get } from "../controllers/parsexml";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [employes, setEmployes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await get(
+        "https://guarded-springs-28309.herokuapp.com/employees"
+      );
+      setEmployes(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Parse XML</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Parse <a href="https://nextjs.org">XML</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Lista de {"  "}
+          <code className={styles.code}>Empleados</code>
         </p>
+        <ul>
+          {employes &&
+            employes.map((item, i) => {
+              return (
+                <li
+                  style={{
+                    listStyle: "none",
+                    boxShadow: "0 2px 5px #0000001a",
+                    padding: 16,
+                    margin: 16,
+                    borderRadius: 16,
+                    display: "flex",
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+                    alignItems: "center",
+                  }}
+                >
+                  <p
+                    style={{
+                      background: "#ccc",
+                      borderRadius: 999,
+                      width: 32,
+                      height: 32,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <b>{item.id}</b>
+                  </p>
+                  <div style={{ marginLeft: 16 }}>
+                    <p>
+                      {item.firstName} {item.lastName}
+                    </p>
+                    <p>{item.emailId}</p>
+                  </div>
+                </li>
+              );
+            })}
+        </ul>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
